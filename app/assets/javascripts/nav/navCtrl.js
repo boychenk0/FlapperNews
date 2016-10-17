094491fff3500816acbb5d1b7fd5cebed6d69251
Yaroslav <boychenk0@ukr.net>
2016-10-17 12:33:02 +0300

    devise-angular

---
+++ app/assets/javascripts/nav/navCtrl.js
@@ -0,0 +1,24 @@
+angular.module('flapperNews')
+.controller('NavCtrl', [
+    '$scope',
+    'Auth',
+    function($scope, Auth){
+        Auth.currentUser().then(function (user){
+            $scope.user = user;
+        });
+        $scope.signedIn = Auth.isAuthenticated;
+        $scope.logout = Auth.logout;
+
+        $scope.$on('devise:new-registration', function (e, user){
+            $scope.user = user;
+        });
+
+        $scope.$on('devise:login', function (e, user){
+            $scope.user = user;
+        });
+
+        $scope.$on('devise:logout', function (e, user){
+            $scope.user = {};
+        });
+    }
+]);
